a3f9ffcb34b2cfa467207bda973446fc7a236c62
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-11-16 21:53:57 +0300

    add processing user input

--- DynamicProgrammingAlgorithms.cpp
+++ DynamicProgrammingAlgorithms.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -39,13 +40,55 @@ vector<vector<int>> readMatrixFromFile(string, int);
 void startFindMinPathInSquare();
 
 
+typedef void(*func)();
+
 void main()
 {
-    startFindMaxPathInTriangle();
-    startFindMinQuantityOfOperations();
-    startAlgorithmNeedlemanWunsch();
-    startSolveTaskAboutBackpack();
-    startFindMinPathInSquare();
+    map<char, func> functions {
+        {'1', startFindMaxPathInTriangle},
+        {'2', startFindMinQuantityOfOperations},
+        {'3', startAlgorithmNeedlemanWunsch},
+        {'4', startSolveTaskAboutBackpack},
+        {'5', startFindMinPathInSquare}
+    };
+
+    char userInput;
+    while (true) {
+
+        cout << "Select a function" << endl;
+        cout << "Find max path in triangle - 1" << endl;
+        cout << "Find min quantity of operations for exponentiation  - 2 " << endl;
+        cout << "Algorithm Needleman Wunsch - 3" << endl;
+        cout << "Solve task about backpack - 4" << endl;
+        cout << "Find min path in Square - 5" << endl;
+        cout << "Exit - 0" << endl;
+
+        cin >> userInput;
+        cout << endl;
+
+        if (userInput == '0')
+        {
+            cout << "Exit" << endl;
+            break;
+        }
+        else if (userInput != '1' && userInput != '2' && userInput != '3' && userInput != '4' && userInput != '5')
+        {
+            cout << "Invalid input" << endl;
+            cout << endl;
+            continue;
+        }
+
+        try
+        {
+            functions[userInput]();
+            cout << endl;
+        }
+        catch (const std::exception& ex)
+        {
+            cout << ex.what() << endl;
+        }
+    }
+
 }
 
 
